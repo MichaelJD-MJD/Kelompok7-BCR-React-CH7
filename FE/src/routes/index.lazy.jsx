efdba4803604cb3211e8a2ee1ef1bc4164113992
David Gurning <davidgurning4@gmail.com>
2024-11-15 10:57:43 +0700

    updated

--- FE/src/routes/index.lazy.jsx
+++ FE/src/routes/index.lazy.jsx
@@ -3,6 +3,7 @@ import "../styles/list-car.css";
 import WhyUs from "../components/User/WhyUs/WhysUs";
 import Header from "../components/User/Header/Header";
 import Testimonial from "../components/User/Testimonial/Testimonial";
+import OurServices from "../components/User/OurServices/OurServices";
 import Banner from "../components/User/Banner/Banner";
 import Faq from "../components/User/FAQ/Faq";
 import Footer from "../components/User/Footer/Footer";
@@ -15,6 +16,7 @@ function Index() {
   return (
     <>
       <Header />
+      <OurServices />
       <WhyUs />
       <Testimonial />
       <Banner />
